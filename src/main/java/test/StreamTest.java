ccfc3f38480348181d8de21bce90d3f4bb294659
myeongjjun <mjk3026@gmail.com>
2022-04-29 02:49:19 +0900

    3.4.3.1 요구사항1 - url path parsing 기능추가

---
+++ src/main/java/test/StreamTest.java
@@ -0,0 +1,58 @@
+package test;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+public class StreamTest {
+    public static void main(String[] args) throws IOException {
+//        inputStreamMain();
+//        InputStreamReaderMain();
+//        bufferedReaderMain();
+        scannerMain();
+    }
+
+    private static void scannerMain() throws IOException {
+        Scanner sc = new Scanner(System.in);
+        System.out.println(sc.next());
+    }
+
+
+    private static void bufferedReaderMain() throws IOException {
+        InputStream in = System.in;
+        InputStreamReader reader = new InputStreamReader(in);
+        BufferedReader br = new BufferedReader(reader);
+
+        String a = br.readLine();
+        System.out.println(a);
+    }
+
+
+    private static void InputStreamReaderMain() throws IOException {
+        InputStream in = System.in;
+        InputStreamReader reader = new InputStreamReader(in);
+
+        char[] a = new char[3];
+        reader.read(a);
+
+        System.out.println(a);
+    }
+
+    private static void inputStreamMain() throws IOException {
+        InputStream in = System.in;
+
+        byte[] a = new byte[3];
+        in.read(a);
+        System.out.println(a[0]);
+        System.out.println(a[1]);
+        System.out.println(a[2]);
+
+
+//        int a;
+//        a = in.read();
+//
+//        System.out.println(a);
+    }
+}
--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,10 +1,9 @@
 package webserver;
 
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.Socket;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -24,6 +23,10 @@ public class RequestHandler extends Thread {
 
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
             // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
+            String filePath = getFilePath(in);
+            log.info("filePath => {}", filePath);
+
+
             DataOutputStream dos = new DataOutputStream(out);
             byte[] body = "Hello World".getBytes();
             response200Header(dos, body.length);
@@ -31,6 +34,38 @@ public class RequestHandler extends Thread {
         } catch (IOException e) {
             log.error(e.getMessage());
         }
+    }
+
+    private String getFilePath(InputStream in) throws IOException {
+        String filePath = "";
+        InputStreamReader reader = new InputStreamReader(in);
+        BufferedReader br = new BufferedReader(reader);
+        String line = br.readLine();
+        while (!"".equals(line) && filePath.equals("")) {
+            if (line == null) {
+                return "";
+            }
+            filePath = extractPath(line);
+            line = br.readLine();
+        }
+        return filePath;
+    }
+
+    private String extractPath(String line) {
+        String[] splitLine = line.split(": |\n");
+        if (splitLine[0].equals("Referer")) {
+            return parseUrlPath(splitLine[1]);
+        }
+        return "";
+    }
+
+    private String parseUrlPath(String url) {
+        Pattern URL_PATH = Pattern.compile("http:\\/\\/.+:[\\d]+(\\/.*)");
+        Matcher matcher = URL_PATH.matcher(url);
+        if (matcher.find()) {
+            return matcher.group(1);
+        }
+        return "";
     }
 
     private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
